b067e7c54cb40c642b066139eeb4e95ac8925620
nicollysirqueira <nicollysirqueiraa@gmail.com>
2025-05-15 17:31:51 -0300

    botão chute

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = "decifre o numero"; //titulo do jogo
 
 let paragrafro = document.querySelector("p") //descrição do jogo
 paragrafro.innerHTML = "Escolha um número entre 1 a 10"; 
+
+function verificarChute() {
+    console.log("botão foi clicado")
+}
