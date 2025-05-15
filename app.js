b86e874597e7cf1a5b7e5025e15b6fa8ff5e3916
nicollysirqueira <nicollysirqueiraa@gmail.com>
2025-05-15 18:03:09 -0300

    ajuste na descrição

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 let numeroSecreto = gerarNumeroAleatorio();
+
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela("h1", "Decifre o número");
+exibirTextoNaTela("h1", "Decifre o número Secreto");
 exibirTextoNaTela("p", "Escolha um número entre 1 a 100");
 
 function verificarChute() {
-    console.log("botão foi clicado!");
+    console.log(numeroSecreto);
 }
 
-function gerarNumeroAleatorio() { //gerar número aleatorio
-    parseInt(Math.random() * 100 + 1 );
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 100 + 1 );
 }
