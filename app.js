0424a00f35e97ec049340e665e1a47b5e9c2eca1
nicollysirqueira <nicollysirqueiraa@gmail.com>
2025-05-15 17:11:34 -0300

    ajuste na descrição escolha

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-let titulo = document.querySelector("h1")
-titulo.innerHTML = "decifre o numero"
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "decifre o numero"; //titulo do jogo
+
+let paragrafro = document.querySelector("p") //descrição do jogo
+paragrafro.innerHTML = "Escolha um número entre 1 a 10"; 
