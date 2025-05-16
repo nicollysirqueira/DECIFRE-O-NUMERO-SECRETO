4f9b2d27016fda393eb308cf0fb912ead88a72fe
nicollysirqueira <nicollysirqueiraa@gmail.com>
2025-05-16 09:30:49 -0300

    ajuste na mesagem ao acertar

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ exibirTextoNaTela("h1", "Decifre o número Secreto");
 exibirTextoNaTela("p", "Escolha um número entre 1 a 100");
 
 function verificarChute() {
-    console.log(numeroSecreto);
+    let chute = document.querySelector("input").value;
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela("Parabéns, você acertou!!");
+    }
 }
 
 function gerarNumeroAleatorio() {
